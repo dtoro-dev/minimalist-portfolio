244818d3106e8890582bc3168bb4d96393539318
Diego Toro Reyes <dtoro.a.s@gmail.com>
2024-07-29 14:48:51 -0400

    cmd keyboard

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -4,3 +4,10 @@ export function formatDate(date: string, locale: string = "es") {
   moment.locale(locale);
   return moment(date).format("MMMM YYYY")
 };
+
+export function truncateText(text: string | null) {
+  if (text && text.length > 140) {
+    return  `${text.substring(0, 137).trim()}...`;
+  }
+  return text;
+}
